8c358240bf88471bf253931f0cd68ea0d9989dce
Sowren Sen <13097375+sowrensen@users.noreply.github.com>
2024-12-17 15:02:08 +0600

    add download functionality

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
 import gradio as gr
 from markitdown import MarkItDown
+from pathlib import Path
+import tempfile
+
+store = {}
+
+
+def upload(filepath):
+    filename = Path(filepath).name
+    store["original_filename"] = Path(filepath).stem
+    return [gr.UploadButton(visible=False),
+            gr.ClearButton(visible=True),
+            gr.Markdown(f"**{filename}**"),
+            gr.Button("Convert", visible=True)]
+
+
+def toggle():
+    return [gr.UploadButton(visible=True),
+            gr.ClearButton(visible=False),
+            gr.Button(visible=False),
+            gr.DownloadButton(visible=False)]
 
 
 def convert(file):
     markitdown = MarkItDown()
     result = markitdown.convert(file.name)
-    return result.text_content
+    content = result.text_content
+    return [gr.Markdown(content),
+            gr.DownloadButton(value=store_markdown(content), visible=True)]
+
+
+def store_markdown(content):
+    output_path = Path(tempfile.gettempdir()) / (store.get("original_filename") + ".md")
+    with open(output_path, "w") as f:
+        f.write(content)
+
+    return output_path
 
 
 with gr.Blocks(title="Markdown Maker",
                theme=gr.themes.Soft(font=gr.themes.GoogleFont("Inter"))) as interface:
-    gr.Markdown("Upload a file and click **Convert** to see the markdown text.")
-    file_input = gr.File(label="Select a file", file_types=[".docx", ".xlsx", ".pdf"])
-    markdown_output = gr.Markdown(label="Preview",
-                                  show_copy_button=True,
-                                  min_height=300,
-                                  max_height=500,
-                                  container=True,
-                                  line_breaks=True)
-    btn = gr.Button("Convert")
-    btn.click(fn=convert, inputs=file_input, outputs=markdown_output)
+    gr.Markdown("✅ Upload a file and click **Convert** to see the markdown text."
+    )
+    md_preview = gr.Markdown(label="Preview",
+                             show_copy_button=True,
+                             min_height=500,
+                             max_height=500,
+                             container=True,
+                             line_breaks=True)
+    fn_preview = gr.Markdown("")
+    with gr.Row():
+        upload_btn = gr.UploadButton(label="Select a file", file_types=[".docx", ".xlsx", ".pdf"])
+        clear_btn = gr.ClearButton(visible=False)
+        convert_btn = gr.Button("Convert", visible=False)
+        download_btn = gr.DownloadButton("Download", visible=False)
 
-    file_input.clear(fn=lambda file: "", inputs=file_input, outputs=markdown_output)
+    upload_btn.upload(fn=upload, inputs=upload_btn, outputs=[upload_btn, clear_btn, fn_preview, convert_btn])
+    clear_btn.add([upload_btn, convert_btn, download_btn, md_preview, fn_preview])
+    clear_btn.click(fn=toggle, outputs=[upload_btn, clear_btn, convert_btn, download_btn])
+    convert_btn.click(fn=convert, inputs=upload_btn, outputs=[md_preview, download_btn])
 
 interface.launch()
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 ## Markdown Maker
 
-This is a Gradio UI wrapper on top of Microsoft's [MarkItDown](https://github.com/microsoft/markitdown) Python library. It allows you to upload a file and click the **Convert** button to see the markdown text.
+This is a Gradio UI wrapper on top of Microsoft's [MarkItDown](https://github.com/microsoft/markitdown) Python library. 
+It allows you to upload a file and click the **Convert** button to see the markdown text.
 
 The UI supports following file uploads:
 
@@ -14,4 +15,11 @@ The UI supports following file uploads:
 
 ### How to use
 
-Create a virtual environment and install the dependencies using `pip install -r requirements.txt`. Then run `python main.py` to start the Gradio UI.
+Create a virtual environment and install the dependencies using `pip install -r requirements.txt`. Then run 
+`python main.py` to start the Gradio UI.
+
+### Functionalities
+
+- Generate markdown from any file
+- Preview markdown
+- Download the generated markdown file
